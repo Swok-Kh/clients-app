5fd84b71d1cc90e8c21d55433a543e8dec22d3e3
Serhii Demchenko HPC <dem.s.vl@gmail.com>
2021-04-12 21:29:00 +0300

    adds app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ClientsList from "./components/clients/ClientsList";
+import Header from "./components/header/Header";
 import Container from "./components/UI/Container";
 
 function App() {
   return (
     <div>
+      <Header />
       <Container>
         <ClientsList />
       </Container>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="h-20 bg-indigo-500">
+      <div className="flex items-center h-full max-w-7xl mx-auto p-1">
+        <h1 className="text-3xl font-semibold text-white">Our Clients</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
